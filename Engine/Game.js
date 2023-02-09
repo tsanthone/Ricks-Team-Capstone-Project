b98723d9aee4c440afad88e9518d3eb56b142e07
Thomas Anthone <tsanthone098@gmail.com>
2023-02-08 18:14:48 -0600

    Update Game.js
    Removed unnecessary variables from the Game.js file.

--- Engine/Game.js
+++ Engine/Game.js
@@ -1,17 +1,7 @@
 class Game{
-    static health = 3;
-    static enterPosition = "north";
-    static damage = 1;
-    static tearRate = 40;
-    static moveSpeed = 200;
-    static score = 0;
-
-    static monsterHealth = 2;
-
     static scenes = [];
     static currentSceneIndex = 0;
     static nextSceneIndex = -1;
-    static numRooms = 0;
 
     static scene(){
         return Game.scenes[Game.currentSceneIndex];
